0c232f7461a82dcc8bb3df1f37a05946c3698a42
Lauren Andrews <53305329+lcandre2@users.noreply.github.com>
2022-12-21 14:01:41 -0500

    modified to deal w path issues

--- icestrainrates/readvelocitydata.m
+++ icestrainrates/readvelocitydata.m
@@ -1,37 +1,38 @@
-function v = readvelocitydata(datasource, path, filetype)
+function v = readvelocitydata(datasource, path1, filetype)
 
 
 if datasource == 1
     
-    cd(path)
+    location1 = [path1, filetype];
     
-    files        = dir(filetype); %will load all the nc4 files in the folder
+    files        = dir(location1); %will load all the nc4 files in the folder
     files        = struct2cell(files);
     files        = files(1, 1:end);
     files        = files';
 
-
+    files2 = [path1, files{1}];
     %This information is specific to promice data
-    v.x      = ncread(files{1}, 'x');
-    v.y      = ncread(files{1}, 'y');
-    [v.x, v.y] = meshgrid(v.x, v.y);
+    v.x      = ncread(files2, 'x');
+    v.y      = ncread(files2, 'y');
+    [v.x, v.y] = meshgrid(v.x, v.y); %these lines make 
     v.x = v.x'; v.y=v.y';
 
     for ii = 1:length(files)
-
-        v.time(:,:,ii)   = ncread(files{ii}, 'time') + datenum(1990,1,1);  %This may need to be changed for python
-        v.tbands(:,:,ii) = ncread(files{ii}, 'time_bnds') + datenum(1990,1,1); %This may need to be changed for python
-        v.e_vel(:,:,ii)  = ncread(files{ii}, 'land_ice_surface_easting_velocity');
-        v.n_vel(:,:,ii)  = ncread(files{ii}, 'land_ice_surface_northing_velocity');
-        v.v_vel(:,:,ii)  = ncread(files{ii}, 'land_ice_surface_vertical_velocity');
+        clear files2
+        files2 = [path1, files{ii}];
+        v.time(:,:,ii)   = ncread(files2, 'time') + datenum(1990,1,1);  %This may need to be changed for python
+        v.tbands(:,:,ii) = ncread(files2, 'time_bnds') + datenum(1990,1,1); %This may need to be changed for python
+        v.e_vel(:,:,ii)  = ncread(files2, 'land_ice_surface_easting_velocity');
+        v.n_vel(:,:,ii)  = ncread(files2, 'land_ice_surface_northing_velocity');
+        v.v_vel(:,:,ii)  = ncread(files2, 'land_ice_surface_vertical_velocity');
 
     end
 
 
-elseif velocitydatasource == 2
+elseif datasource == 2
 %
 
-elseif velocitydatasource == 3
+elseif datasource == 3
 
 
 
